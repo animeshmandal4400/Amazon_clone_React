9b5734bb67519da244cb1682a6a31a1bfb346f3c
ANIMESH MANDAL <animeshmandal4400@gmail.com>
2022-06-07 09:35:40 +0530

    commit changes

--- src/components/Register/register.jsx
+++ src/components/Register/register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "./register.css"
-//import { auth } from '../../firebase';
+import { auth } from '../../firebase';
 
 const Register = () => {
     //const history = useNavigate();
